100afc6c807d9c3690614eba65e3c9f43b50238b
taymara25 <taymara20@gmail.com>
2021-06-08 09:37:39 -0500

    Add project files.

---
+++ Facturation_system/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Facturation_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
